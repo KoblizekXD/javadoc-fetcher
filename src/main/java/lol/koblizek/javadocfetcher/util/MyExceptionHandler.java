4492908d8a88e8f9cbbce1c878a2f136b5202f8d
KoblizekXD <janprokupek@centrum.cz>
2025-01-04 09:50:51 +0100

    Better exception handling

--- src/main/java/lol/koblizek/javadocfetcher/util/MyExceptionHandler.java
+++ src/main/java/lol/koblizek/javadocfetcher/util/MyExceptionHandler.java
@@ -2,14 +2,22 @@ package lol.koblizek.javadocfetcher.util;
 
 import lol.koblizek.javadocfetcher.models.http.ErrorResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class MyExceptionHandler {
+    
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
         return ResponseEntity.status(500).body(new ErrorResponse(e.getMessage(), 
                 e.getStackTrace()));
+    }
+    
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleException(HttpMediaTypeNotSupportedException e) {
+        return ResponseEntity.status(415).body(new ErrorResponse("Unsupported media type, please use application/json", 
+                new StackTraceElement[0]));
     }
 }
